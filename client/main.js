eb0c9342eccc14e0421c2d3c6bab687c88711200
MarnatalChristian <christian.kodz@gmail.com>
2020-08-13 05:38:03 +0700

    layouting

---
+++ client/main.js
@@ -0,0 +1,45 @@
+// $(document).ready(() => {
+//     Auth()
+
+// })
+
+// function loginPage() {
+//     $('.signup').hide()
+//     $('.signin').hide()
+//     $('#navbarDropdownMenuLink').hide()
+//     $('#logoutbtn').hide()
+// }
+
+// function homePage() {
+//     $('.signup').hide()
+//     $('.signin').hide()
+//     $('.fixed-form').hide()
+// }
+
+// function registerPage() {
+//     $('.signup').show()
+//     $('.signin').hide()
+//     $('#navbarDropdownMenuLink').hide()
+//     $('#logoutbtn').hide()
+// }
+
+// function addForm() {
+//     $('.fixed-form').hide()
+// }
+
+// function cancelbtn() {
+//     $('.fixed-form').hide()
+// }
+
+// function Auth() {
+//     if (localStorage.access_token) {
+//         homePage()
+//     } else {
+//         loginPage()
+//     }
+// }
+
+// $('#logoutbtn').click(() => {
+//     localStorage.clear()
+//     Auth()
+// }) 
